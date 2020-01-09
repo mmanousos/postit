156ceb775e37e45d3d3997d53e1b502d4c99c58f
Melissa Manousos <mmanousos@gmail.com>
2020-01-09 14:13:18 -0800

    Adds ability to create comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
- 
-  resources :posts
+ root to: 'posts#index'
+
+  resources :posts, except: [:destroy] do
+    resources :comments, only: [:create]
+  end 
   resources :categories, only: [:new, :create, :show]
- 
-  root 'posts#index'
 end
