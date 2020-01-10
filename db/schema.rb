437b2bb8a6002fcdbc931132a3c3e8008fe3c7ed
Melissa Manousos <mmanousos@gmail.com>
2020-01-10 10:28:21 -0800

    Adds authentication to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_225121) do
+ActiveRecord::Schema.define(version: 2020_01_10_180658) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_01_07_225121) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
